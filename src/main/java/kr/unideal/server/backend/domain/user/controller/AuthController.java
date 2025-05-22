a2691d72d5fcfc5425c7ceb3d72b050ecd2d5782
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-22 16:03:53 +0900

    ✨ feat: auth controller 구현

--- src/main/java/kr/unideal/server/backend/domain/user/controller/AuthController.java
+++ src/main/java/kr/unideal/server/backend/domain/user/controller/AuthController.java
@@ -1,85 +1,72 @@
 package kr.unideal.server.backend.domain.user.controller;
 
-import jakarta.validation.Valid;
-import kr.unideal.server.backend.domain.user.dto.LogInRequestDTO;
-import kr.unideal.server.backend.domain.user.dto.LogInResponseDTO;
-import kr.unideal.server.backend.domain.user.dto.SignUpRequestDTO;
+import jakarta.mail.MessagingException;
+import kr.unideal.server.backend.domain.user.dto.request.EmailSendRequest;
+import kr.unideal.server.backend.domain.user.dto.request.EmailVerifyRequest;
+import kr.unideal.server.backend.domain.user.dto.request.LoginRequest;
+import kr.unideal.server.backend.domain.user.dto.response.LoginResponse;
+import kr.unideal.server.backend.domain.user.exception.NotVerifiedException;
+import kr.unideal.server.backend.domain.user.service.EmailService;
 import kr.unideal.server.backend.domain.user.service.UserService;
+import kr.unideal.server.backend.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
+    private final EmailService emailService;
 
-    //signup 페이지 GET
-    @GetMapping("/signup")
-    public String signupPage(Model model) {
-        model.addAttribute("signUpRequestDTO", new SignUpRequestDTO());
-        return "signup";
+    // 이메일 전송하기
+    @PostMapping("/email")
+    public ApiResponse<String> email(@RequestBody EmailSendRequest request) throws MessagingException {
+        emailService.sendEmail(request.getEmail());
+        return ApiResponse.ok("성공적으로 메일이 전송되었습니다.");
+    }
+
+    // 이메일 검증하기
+    @PostMapping("/email/verify")
+    public ApiResponse<String> verify(@RequestBody EmailVerifyRequest request) {
+        if (!emailService.verifyCode(request.getEmail(), request.getCode())){
+            throw new NotVerifiedException("인증번호가 틀렸습니다.");
+        }
+
+        return ApiResponse.ok("메일이 인증되었습니다.");
+    }
+
+    @PostMapping("/login")
+    public ApiResponse<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ApiResponse.ok(userService.login(request));
+    }
+
+    @PostMapping("/logout")
+    public ApiResponse<String> logout() {
+        userService.logout();
+        return ApiResponse.ok("성공적으로 로그아웃 되었습니다.");
     }
 
 
-    //login 페이지 Get
-    @GetMapping("/login")
-    public String loginPage(Model model) {
-        model.addAttribute("logInRequestDTO", new LogInRequestDTO());
-        return "login";
-    }
-
-    @PostMapping("/auth/signup")
-    public String signup(@Valid @ModelAttribute SignUpRequestDTO signUpRequestDTO,
-                         BindingResult bindingResult,
-                         Model model) {
-
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("error", "입력값을 다시 확인해주세요.");
-            return "signup";
-        }
-
-        try {
-            userService.register(signUpRequestDTO);
-        } catch (IllegalArgumentException e) {
-            model.addAttribute("error", e.getMessage());
-            return "signup";
-        }
-
-        return "redirect:/login";
-    };
-
-    @PostMapping("/auth/login")
-    public ResponseEntity<LogInResponseDTO> login(@RequestBody @Valid LogInRequestDTO loginRequestDTO) {
-        try {
-            LogInResponseDTO response = userService.loginMember(loginRequestDTO);
-            return ResponseEntity.ok(response); // ✅ 토큰 포함 응답
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build(); // 또는 에러 메시지 반환
-        }
-    }
-
-//    @PostMapping("/auth/login")
-//    public String login(@ModelAttribute @Valid LogInRequestDTO loginRequestDTO,
-//                        BindingResult bindingResult,
-//                        Model model,
-//                        HttpSession session) {
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("error", "입력값을 다시 확인해주세요.");
-//            return "login";
-//        }
-//
-//        try {
-//            User user = userService.login(loginRequestDTO);
-//            session.setAttribute("user", user); // 로그인 상태 저장
-//            return "redirect:/"; // 홈 또는 마이페이지 등으로 이동
-//        } catch (IllegalArgumentException e) {
-//            model.addAttribute("error", e.getMessage());
-//            return "login";
-//        }
+//    @DeleteMapping()
+//    public ApiResponse<String> delete(@AuthenticationPrincipal CustomUserDetails userDetails) {
+//        userService.deleteuser(userDetails.getId());
+//        return ApiResponse.ok("성공적으로 탈퇴되었습니다.");
 //    }
+
+//    @PostMapping("/request")
+//    public ApiResponse<String> requestReset(@AuthenticationPrincipal CustomUserDetails userDetails) throws MessagingException {
+//        userService.requestPasswordChange(userDetails.getId());
+//        return ApiResponse.ok("성공적으로 비밀번호 요청메일이 전송되었습니다.");
+//
+//    }
+
+//    @PutMapping("/reset-password")
+//    public ApiResponse<String> resetPassword(@RequestParam String token, @AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody userPasswordChangeRequest request) {
+//
+//        userService.changePassword(token, userDetails.getId(), request.getNewPassword(), request.getConfirmPassword());
+//        return ApiResponse.ok("성공적으로 비밀번호가 변경되었습니다.");
+//    }
+
 }
