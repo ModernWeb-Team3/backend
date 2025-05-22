78430a23e34f76b96b1d80350b0b47a617830402
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-22 16:02:59 +0900

    ✨ feat: email config추가, 검증 객체 구현

---
+++ src/main/java/kr/unideal/server/backend/domain/user/entity/VerificationCode.java
@@ -0,0 +1,30 @@
+package kr.unideal.server.backend.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VerificationCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    private String email;
+
+    private String code;
+
+    private LocalDateTime expiresTime;
+}
