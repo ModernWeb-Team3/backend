6570c2f904c51bb52b3fa851eac82fb2ce51b072
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-22 17:35:49 +0900

    ✨ feat: email dto, repo구현

---
+++ src/main/java/kr/unideal/server/backend/domain/user/dto/request/EmailSendRequest.java
@@ -0,0 +1,13 @@
+package kr.unideal.server.backend.domain.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class EmailSendRequest {
+
+    @Schema(description = "이메일", example = "example@gmail.com")
+    private String email;
+}
