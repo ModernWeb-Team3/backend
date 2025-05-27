b768e7adfa096bbaa4387dbc08d46868a74c9a13
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-27 13:49:56 +0900

    ✨ feat: 변수 설정

--- src/main/java/kr/unideal/server/backend/global/properties/Constants.java
+++ src/main/java/kr/unideal/server/backend/global/properties/Constants.java
@@ -1,6 +1,8 @@
-package kr.unideal.server.backend.config.properties;
+package kr.unideal.server.backend.global.properties;
 
 public class Constants {
     public static final String AUTH_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+
 }
