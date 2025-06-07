f214d171abadaa00555bac064d4aff9fb291059f
hwangjiyoung02 <hjy020714@gmail.com>
2025-06-08 00:29:11 +0900

    🔥 fix: OpenAPIConfig에 bear token 수정

--- src/main/java/kr/unideal/server/backend/openapi/OpenAPIConfig.java
+++ src/main/java/kr/unideal/server/backend/openapi/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package kr.unideal.server.backend.openapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -9,7 +12,14 @@ import org.springframework.context.annotation.Configuration;
                 title = "Unideal API 명세서",
                 description = "Unideal의 API 명세서입니다.",
                 version = "v1"
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth") // 👈 모든 API에 기본 적용
+)
+@SecurityScheme(
+        name = "bearerAuth",                // 👈 위 SecurityRequirement와 일치해야 함
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"               // 👈 표기용, 실제 동작과는 무관
 )
 @Configuration
 public class OpenAPIConfig {
