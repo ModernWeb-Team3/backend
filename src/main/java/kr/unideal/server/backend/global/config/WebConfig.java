8e24d18af6ac4c8fe3248b1e4aa024eaa564e687
hwangjiyoung02 <hjy020714@gmail.com>
2025-06-08 12:00:38 +0900

    🔥 fix: security 변경

--- src/main/java/kr/unideal/server/backend/global/config/WebConfig.java
+++ src/main/java/kr/unideal/server/backend/global/config/WebConfig.java
@@ -23,23 +23,23 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(currentUserIdArgumentResolver);
     }
 
-
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns(
-                        "http://localhost:3000",
-                        "http://localhost:8080",
-                        "https://staging-api.unideal.kr",
-                        "https://staging.unideal.kr",
-                        "https://staging-front.unideal.kr",
-                        "https://unideal.kr",
-                        "https://front.unideal.kr"
-                )
-                .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns(
+//                        "http://localhost:3000",
+//                        "http://localhost:8080",
+//                        "https://staging-api.unideal.kr",
+//                        "https://staging.unideal.kr",
+//                        "https://staging-front.unideal.kr",
+//                        "https://unideal.kr",
+//                        "https://front.unideal.kr"
+//                )
+//                .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
 
 
 }
